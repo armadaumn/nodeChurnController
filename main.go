ab81600bca895954e1e8ad47a293809e07beac66
lei6669 <lh@dhcp-wifi-8021x-155-41-85-145.bu.edu>
2021-10-28 20:27:19 -0400

    a

--- main.go
+++ main.go
@@ -190,7 +190,6 @@ func readClientCSV() []string {
 		log.Println(err)
 		os.Exit(0)
 	}
-	fmt.Println(addrLines)
 	for i := 0; i < len(addrLines); i++ {
 		addresses = append(addresses, addrLines[i][0])
 	}
